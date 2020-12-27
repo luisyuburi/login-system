ff93ce60d328d9025eef613964d7c496d4c97b88
Luis Alejandro Yuburi <luisyuburi@outlook.com>
2020-12-27 18:14:27 -0500

    make it better

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,4 +12,21 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-require('./components/Example');
+// equire('./components/Example');
+
+$(document).ready(function () {
+    let selected = 1
+    const imagesend = $("#img-send");
+    const imageget = $("#img-get");
+    $(".radio-button-option").focus(function () {
+        let action =  $(this).data("action");
+        selected = $(this).data("option");
+        if(action === "get") {
+            imageget.attr("src", "/img/get" + selected + ".png")
+        }
+        else if(action === "send") {
+            imagesend.attr("src", "/img/send" + selected + ".png")
+        }
+        console.log($(this).data("action"));
+    })
+});
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,109 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+
+
+                        <ul class="nav nav-tabs nav-fill" id="myTab" role="tablist">
+                            <li class="nav-item" role="presentation">
+                                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab"
+                                    aria-controls="recibir" aria-selected="true">Recibir</a>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab"
+                                    aria-controls="enviar" aria-selected="false">Enviar</a>
+                            </li>
+
+                        </ul>
+                        <div class="tab-content text-center" id="myTabContent">
+                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="recibir-tab">
+                                <div class="btn-group btn-group-toggle mt-3" data-toggle="buttons">
+
+                                    <label class="btn btn-secondary active">
+                                        <input type="radio" name="options" id="option1" class="radio-button-option" checked
+                                            data-option="1" data-action="get"> 1
+                                    </label>
+                                    <label class="btn btn-secondary">
+                                        <input type="radio" name="options" id="option2" class="radio-button-option"
+                                            data-option="2" data-action="get"> 2
+                                    </label>
+                                    <label class="btn btn-secondary">
+                                        <input type="radio" name="options" id="option3" class="radio-button-option"
+                                            data-option="3" data-action="get"> 3
+                                    </label>
+                                    <label class="btn btn-secondary">
+                                        <input type="radio" name="options" id="option4" class="radio-button-option"
+                                            data-option="4" data-action="get"> 4
+                                    </label>
+                                    <label class="btn btn-secondary">
+                                        <input type="radio" name="options" id="option5" class="radio-button-option"
+                                            data-option="5" data-action="get"> 5
+                                    </label>
+
+                                </div>
+
+
+                                <div>
+                                    <img src="/img/get1.png"" class="img-fluid" id="img-get" />
+                                </div>
+
+                            </div>
+                            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="enviar-tab">
+
+                                <div class="tab-content text-center" id="myTabContent">
+                                    <div class="tab-pane fade show active" id="home" role="tabpanel"
+                                        aria-labelledby="recibir-tab">
+                                        <div class="btn-group btn-group-toggle mt-3" data-toggle="buttons">
+
+                                            <label class="btn btn-secondary active">
+                                                <input type="radio" name="options" id="option1" class="radio-button-option"
+                                                    checked data-option="1" data-action="send"> 1
+                                            </label>
+                                            <label class="btn btn-secondary">
+                                                <input type="radio" name="options" id="option2" class="radio-button-option"
+                                                    data-option="2" data-action="send"> 2
+                                            </label>
+                                            <label class="btn btn-secondary">
+                                                <input type="radio" name="options" id="option3" class="radio-button-option"
+                                                    data-option="3" data-action="send"> 3
+                                            </label>
+                                            <label class="btn btn-secondary">
+                                                <input type="radio" name="options" id="option4" class="radio-button-option"
+                                                    data-option="4" data-action="send"> 4
+                                            </label>
+                                            <label class="btn btn-secondary">
+                                                <input type="radio" name="options" id="option5" class="radio-button-option"
+                                                    data-option="5" data-action="send"> 5
+                                            </label>
+                                        </div>
+
+                                        <div>
+                                            <img src="/img/send1.png" class="img-fluid" id="img-send" />
+                                        </div>
+                                    </div>
+                                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="enviar-tab">...
+                                    </div>
+                                </div>
+
+
+                            </div>
                         </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
